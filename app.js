c0ce9fb80853ecd6337e1a40d950382335fa416c
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2026-02-21 19:06:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/* =============================
-   MongoDB Connection
-============================= */
 
-// إذا شغال Docker رح ياخد MONGO_URI من compose
-// إذا شغال محلي رح يستخدم localhost
 const mongoUri =
   process.env.MONGO_URI || "mongodb://localhost:27017/money-track";
 
@@ -23,23 +18,17 @@ mongoose
     process.exit(1);
   });
 
-/* =============================
-   Middleware
-============================= */
+
 
 app.use(express.json());
 
-/* =============================
-   Test Route
-============================= */
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Money Track API is running 🚀" });
 });
 
-/* =============================
-   Start Server
-============================= */
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
